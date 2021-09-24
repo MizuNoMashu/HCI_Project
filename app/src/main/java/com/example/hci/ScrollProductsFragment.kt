c7f117ee4b4dd01102bf70929c3d4d5c656e5ec2
MizuNoMashu <matteo.marinacci@gmail.com>
2021-09-24 10:54:01 +0200

    fixed ScrollProductsFragment push

--- app/src/main/java/com/example/hci/ScrollProductsFragment.kt
+++ app/src/main/java/com/example/hci/ScrollProductsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.hci.data.ItemData
@@ -17,6 +18,7 @@ class ScrollProductsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        if(logged_user == null) NavHostFragment.findNavController(this).navigate(R.id.action_scroll_products_to_loginFragment)
         val binding = inflater.inflate(R.layout.fragment_scroll_products,container,false)
         val myItems = ItemData().loadItem()
         val recyclerView = binding.findViewById<RecyclerView>(R.id.scroll_products)
